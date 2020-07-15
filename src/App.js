e20e3a082ccc9df2635bcb75aacc80b09d6a0451
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-15 19:27:39 -0300

    Add body and animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button, Heading, Grommet } from "grommet";
+import React, { useState } from "react";
+import { Box, Button, Collapsible, Heading, Grommet } from "grommet";
 import { Notification } from "grommet-icons";
+
 import Header from "./components/Header";
 
 const theme = {
@@ -17,14 +18,38 @@ const theme = {
 };
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <Grommet theme={theme}>
-      <Header>
-        <Heading level="3" margin="none">
-          My App
-        </Heading>
-        <Button icon={<Notification />} onClick={() => {}} />
-      </Header>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <Header>
+          <Heading level="3" margin="none">
+            My App
+          </Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => setShowSidebar(!showSidebar)}
+          />
+        </Header>
+        <Box direction="row" flex>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          <Collapsible direction="horizontal" open={showSidebar}>
+            <Box
+              flex
+              width="medium"
+              background="light-2"
+              elevation="small"
+              align="center"
+              justify="center"
+            >
+              sidebar
+            </Box>
+          </Collapsible>
+        </Box>
+      </Box>
     </Grommet>
   );
 }
